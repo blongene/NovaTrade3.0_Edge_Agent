cbe0bc4a5c1753e8e8b1c07aa1bf76cc0b6819c5
blongene <blongene@gmail.com>
2025-10-11 22:48:50 -0400

    Delete run_rebalance.bat

--- run_rebalance.bat
+++
@@ -1,6 +0,0 @@
-
-@echo off
-REM Launch Rebalance Engine in local environment
-set SHEET_URL=https://docs.google.com/spreadsheets/d/1rE6rbUnCPiL8OgBj6hPWNppOV1uaII8im41nrv-x1xg/edit
-python rebalance_main.py
-pause
